5904f22b5f6c8b13bda7f6bd7cf93df438d26c79
Yunesh Shrestha <yuneshshrestha24@gmail.com>
2023-10-08 07:58:29 +0545

    file upload bug has been fixed

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -7,11 +7,37 @@ use Illuminate\Http\Request;
 class FileController extends Controller
 {
     function upload(Request $req){
-        $result =  $req->file('file')->store('apiDocs/photos');
-        return ["Result"=>$result];
+        if(!$req->hasFile('file')){
+            return response()->json(["Result"=>"No file uploaded"], 400);
+        }
+
+        // fetch file
+        $file = $req->file('file');
+        // get file extension
+        $extension = $file->getClientOriginalExtension();
+        // filename to store
+        $filename = time().'.'.$extension;
+        // upload file
+        $file->move('apiDocs/photos', $filename);
+     $filename = 'apiDocs/photos/'.$filename;
+        return ["Result"=>asset($filename)];
     }
     function video(Request $req){
+        if(!$req->hasFile('file')){
+            return response()->json(["Result"=>"No file uploaded"], 400);
+        }
         $result =  $req->file('file')->store('apiDocs/videos');
-        return ["Result"=>$result];
+
+        // fetch file
+        $file = $req->file('file');
+        // get file extension
+        $extension = $file->getClientOriginalExtension();
+        // filename to store
+        $filename = time().'.'.$extension;
+        // upload file
+        $file->move('apiDocs/videos', $filename);
+        $filename = 'apiDocs/videos/'.$filename;
+        
+        return ["Result"=>asset($filename)];
     }
 }
